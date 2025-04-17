7c72f28486d7191870db09f48b41da3d9b1b32ae
RaflyStillLearn <raflyfirmansyah1209@gmail.com>
2025-04-17 22:30:09 +0700

    Variabel.py
    Rangkuman pembelajaran saya di hari 1

---
+++ Hari1-variabel/variabel.py
@@ -0,0 +1,15 @@
+# Hari 1 - Variabel Python 
+
+Hari ini saya belajar dasar-dasar variabel di Python.
+
+## Materi :
+- Cara membuat Variabel 
+- Tipe data dasar (int, str, float, bool)
+- Print ke layar 
+
+## Contoh Kode 
+'''python
+nama = "Rafly"
+umur = 17
+hobi = "Baca buku"
+print ("Halo nama saya", nama, "umur saya", umur, "tahun", "hobi saya", hobi)
