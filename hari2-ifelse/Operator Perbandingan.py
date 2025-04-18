05a49fb847d27f1ec1516264ad88c95654794db9
RaflyStillLearn <raflyfirmansyah1209@gmail.com>
2025-04-18 13:44:12 +0700

    Operator Perbandingan.py
    Berisikan materi dan contoh penerapan operator perbandingan dalam python.

---
+++ hari2-ifelse/Operator Perbandingan.py
@@ -0,0 +1,19 @@
+# Hari 2 - Operator Perbandingan 
+
+Hari ini saya mempelajari jenis-jenis operator perbandingan dalam python.
+
+## Materi
+- Mengerti cara membaca operator perbandingan dalam python serta penggunaannya.
+- Mengerti operator perbandingan (==,!=, >, <, >=, <=,)
+
+## Contoh Kode
+'''Python
+x = 7
+y = 4
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
