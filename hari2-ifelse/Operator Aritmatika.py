2dc744d9fd6ee9c5cd0377378189d6489648a7cc
RaflyStillLearn <raflyfirmansyah1209@gmail.com>
2025-04-18 13:37:32 +0700

    Operator Aritmatika.py
    Berisikan materi dan penerapan dari operator aritmatika dalam pyhton.

---
+++ hari2-ifelse/Operator Aritmatika.py
@@ -0,0 +1,21 @@
+# Hari 2 - Operator
+
+Hari ini saya mempelajari dasar-dasar operator aritmatika di bahasa pemrograman python.
+
+## Materi
+- Mengerti cara membaca operator aritmatika di pyhton.
+- Mengetahui fungsi-fungsi dari tiap operator.
+- Menerapkan tiap-tiap operator aritmatika dalam sebuah pemrograman.
+
+## Contoh Kode
+'''Python
+x = 7
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
